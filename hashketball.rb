84bb725bd44501bc6dfad93ed8f35d26e60dce36
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-02-22 14:41:43 -0800

    commit tree baf9297c1eea3aa76335db5f87ca07e3bd67e8e2

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,5 @@
-# Write your code below game_hash
+require "pry"
+
 def game_hash
   {
     home: {
@@ -126,4 +127,67 @@ def game_hash
   }
 end
 
-# Write code here
+def find_player player_name
+  home_fplayer = game_hash[:home][:players].filter {|player| player[:player_name] === player_name}
+  away_fplayer = game_hash[:away][:players].filter {|player| player[:player_name] === player_name}
+
+  if home_fplayer[0]
+    home_fplayer[0]
+  else
+    away_fplayer[0]
+  end
+end
+
+def num_points_scored player_name
+  find_player(player_name)[:points]
+end
+
+def shoe_size player_name
+  find_player(player_name)[:shoe]
+end
+
+def team_colors team_name
+  if game_hash[:home][:team_name] === team_name
+    game_hash[:home][:colors]
+  else #away team
+    game_hash[:away][:colors]
+  end
+end
+
+def team_names
+  [game_hash[:home][:team_name], game_hash[:away][:team_name]]
+end
+
+def player_numbers team_name
+  # returns an Array of jersey numbers for that team
+  if game_hash[:home][:team_name] === team_name
+    game_hash[:home][:players].map {|player| player[:number]}
+  else #away team
+    game_hash[:away][:players].map {|player| player[:number]}
+  end
+end
+
+def player_stats player_name
+  find_player(player_name)
+end
+
+def find_player_max shoe_or_points
+  all_nums = []
+  all_nums << game_hash[:home][:players].map {|player| player[shoe_or_points]}
+  all_nums << game_hash[:away][:players].map {|player| player[shoe_or_points]}
+  home_found_player = game_hash[:home][:players].find {|player| player[shoe_or_points] === all_nums.join(",").split(",").max.to_i}
+  away_found_player = game_hash[:away][:players].find {|player| player[shoe_or_points] === all_nums.join(",").split(",").max.to_i}
+  home_found_player ? home_found_player : away_found_player
+end
+
+def big_shoe_rebounds
+  find_player_max(:shoe)[:rebounds]
+end
+
+def most_points_scored
+  find_player_max(:points)[:name]
+end
+
+def winning_team
+  pp find_player_max(:points)
+end
